76f54a671bfb0a637db03a18c927098f0b516015
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-13 10:30:12 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
 
-  self.current_user
-
+  self.current_user(session)
+    User.find_by(id: session[:user_id])
   end
 
-  self.is_logged_in?
-
+  self.is_logged_in?(session)
+    !!session[:user_id]
   end
-  
+
 end
