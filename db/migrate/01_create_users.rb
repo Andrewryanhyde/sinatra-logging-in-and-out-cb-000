82562c6fe8c1420320d1ab823320933bea34e3c1
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-13 10:11:45 +0000

    automatically backed up by learn

--- db/migrate/01_create_users.rb
+++ db/migrate/01_create_users.rb
@@ -1,8 +1,8 @@
-class CreateUsers < Activerecord::Migration 
-  def change 
-    create_table :users do |t| 
-      t.string :username 
-      t.string :password 
+class CreateUsers < Activerecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password
       t.decimal :balance
     end
   end
